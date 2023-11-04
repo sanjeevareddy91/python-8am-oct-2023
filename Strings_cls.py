ed041fe270fc9f4e8d0da361b88c61dccab4d3ed
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2023-11-04 09:03:39 +0530

    04-11-2023 Class files pushed

--- Strings_cls.py
+++ Strings_cls.py
@@ -66,7 +66,7 @@ str1 = "Python is a programming language."
 
 
 # Negative Indexing -- It will start from -1..
-str1 = "Python is a programming language."
+# str1 = "Python is a programming language."
 
 # print(str1[-1])
 
@@ -100,7 +100,7 @@ str1 = "Python is a programming language."
 
 # Immutable -- Those Which we cannot change once the declartion is done.
 
-str1 = "Python is a programming language."
+# str1 = "Python is a programming language."
 
 
 # str1[0] = 'p' # will throw the error as strings are immutable..
@@ -110,23 +110,153 @@ str1 = "Python is a programming language."
 # Repetition(*). -- Repeating the same string multiple number of times.
 
 
-hero = "Balayya"
+# hero = "Balayya"
 
-slogan = " Jai Balayya"
+# slogan = " Jai Balayya"
 
 # print(hero + slogan)
 
-print(slogan*5)
+# print(slogan*5)
 
 
-# dir -- WIll list what are all the methods that i can perform on the value..
+# # dir -- WIll list what are all the methods that i can perform on the value..
 
-print(dir(str))
+# print(dir(str))
 
 
-# Multiline comments
-"""
-sakjsjkajksa
-sasjakjsakjsajk
-hsjhasaa
-"""
+# # Multiline comments
+# """
+# sakjsjkajksa
+# sasjakjsakjsajk
+# hsjhasaa
+# """
+
+# print(dir(str))
+
+# startswith,endswith,isalpha,isalnum,isdigit,islower,isupper,lower,upper,title,capitalize,swapcase,index,find,count,replace,split,strip,zfill,join,format.
+
+
+
+dialogue = "Dont trouble the trouble if you toruble the truoble trouble troubles you i am not the trouble i am the truth"
+
+#startswith() -- Its just for checking whwther the string is starting with what we specified or not.
+
+#endswith() -- Its just for checking whwther the string is ending with what we specified or not.
+
+# print(dialogue.startswith('D'))
+
+# print(dialogue.startswith("Don't"))
+
+# print(dialogue.endswith('truth'))
+
+# print(dialogue.endswith('Truth'))
+
+
+# isalpha() - It will check whwther everything inside the string are alphabets only..
+
+# print(dialogue.isalpha())
+
+# email = "gsanjeev@gmail.com"
+
+# print(dialogue.isalpha())
+
+
+# first_name = "Sanjeev"
+
+# print(first_name.isalpha())
+
+
+# isalnum() -- It will check whwther everything inside the string are alphanumeric values..
+
+
+# pancard = "KAG62HTE8y"
+
+# print(pancard.isalnum())
+
+# aadhar = "82982919821912"
+
+# print(aadhar.isalnum())
+
+# Firstname = "Sanjeeva"
+# print(Firstname.isalnum())
+
+
+# pancard = "KAG62 HTE8y"
+
+# print(pancard.isalnum())
+
+
+# isdigit() - It is to check whether everything inside the string is numbers only..
+
+# mobile = "821929121098"
+
+# print(mobile.isdigit())
+
+# mobile = "8219291 21098"
+
+# print(mobile.isdigit())
+
+# islower() - Its to check whwther everything inside the string is lowercase only..
+# isupper() - Its to check whwther everything inside the string is uppercase only..
+
+# sample1 = "PYTHON PROGRAMMING 261721"
+
+# print(sample1.isupper())
+
+# sample1 = "django 261721"
+
+# print(sample1.islower())
+
+# lower() - Its for converting all the alphabets into lower case..
+
+# upper() - Its for converting all the alphabets into upper case..
+
+# email = "GSANjeev123@GMail.com"
+
+# print(email.lower())
+
+# print(email.upper())
+
+# title() -- Each word starting alphabet will be converted into uppercase inside the string..
+
+# dialogue = "Dont trouble the trouble if you tROUBLE the truoble trouble troubles you i am not the trouble i am the truth"
+
+# print(dialogue.title())
+
+# dialogue = "Dont trouble the 6217trouble if @#@#you tROUBLE the truoble trouble troubles you i am not the trouble i am the truth"
+
+# print(dialogue.title())
+
+# # capitalize - Only starting charcater of the string will be converted to uppercase..
+
+# print(dialogue.capitalize())
+
+# # swapcase() -- Converting lowercase to upper and upper to lowercase..
+
+# print(dialogue.swapcase())
+
+# print(dialogue)
+
+# index -- It will return the index value of the element inside the string..
+
+sample1 = "Django2121 is a we21b framework 72for python"
+
+# print(sample1.index('f'))
+
+# print(sample1.index('is'))
+
+# print(sample1.index('b frame'))
+
+# print(sample1.index('72'))
+
+# print(sample1.index('o'))
+
+
+# count -- It will return how many times a particular elements repeated inside the string..
+
+# print(sample1.count('o'))
+
+# Task:
+# str1 = "Djang2121 is a we21b framework 72for python"
+# Enter which elements: 'o'
+# Input("O has occcured 4 time, which occurece you want..")
