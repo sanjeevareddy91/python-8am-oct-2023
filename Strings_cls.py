d6967c22568f10fe18b794668482f0bdc6d0bad6
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2023-11-06 09:06:34 +0530

    Added 06-11-2023 Files

--- Strings_cls.py
+++ Strings_cls.py
@@ -239,7 +239,7 @@ dialogue = "Dont trouble the trouble if you toruble the truoble trouble troubles
 
 # index -- It will return the index value of the element inside the string..
 
-sample1 = "Django2121 is a we21b framework 72for python"
+# sample1 = "Django2121 is a we21b framework 72for python"
 
 # print(sample1.index('f'))
 
@@ -260,3 +260,121 @@ sample1 = "Django2121 is a we21b framework 72for python"
 # str1 = "Djang2121 is a we21b framework 72for python"
 # Enter which elements: 'o'
 # Input("O has occcured 4 time, which occurece you want..")
+
+# find -- 
+
+# sample1 = "Django2121 is a we21b framework 72for python"
+
+
+# print(sample1.find('f'))
+
+# print(sample1.find('is'))
+
+# print(sample1.find('b frame'))
+
+# print(sample1.find('72'))
+
+# print(sample1.find('o'))
+
+
+
+# print(sample1.index('z')) # It will throw the error as the element is not present inside the string..
+
+# print(sample1.find('z'))
+
+# replace,split,strip,zfill,join,format.
+
+# replace() - Its for replacing the element occurence with someother element inside the string..
+
+
+dialogue = "Dont trouble the trouble if you trouble the trouble trouble troubles you i am not the trouble i am the truth"
+
+# print(dialogue.replace('trouble','problem'))
+
+# print(dialogue.replace('trouble','problem',3))
+
+
+# split() -- Its for splitting the string into multiple strings based on the space(by default)..
+
+# print(dialogue.split()) # splits the string into multiple strings based upon spaces..
+
+
+# print(dialogue.split('o'))
+
+# print(dialogue.split('trouble'))
+
+# print(dialogue.split('z'))
+
+# strip() -- Its for removing the escape sequences from the string..
+
+str1 = "\nPython is a programming \nlanguage used for web\t development using \n django framework\n"
+
+# print(str1)
+
+# print(str1.strip())
+
+# print(str1.rstrip())
+
+# print(str1.lstrip())
+
+
+# email = "sanjeev.reddy@gmail.com"
+
+# # print(email.split('@')[0])
+
+
+# print(email.strip('@gmail.com'))
+
+# print(email.rstrip('@gmail.com'))
+
+# zfill -- Zero Fills(Filling the string with 0's towards left side..)
+
+# str1 = "82188921"
+
+# print(str1.zfill(10))
+
+# join() -- Its for joining the multiple strings into single string or adding an element to each element inside the string..
+# 
+
+
+# str1 = "Python"
+
+# print("@".join(str1))
+
+
+# list1 = ["python","Django","Datascience"]
+
+# print(" ".join(list1))
+
+# format..
+
+# str1 = "Use OTP 781213 for your purchase at Flipkart.com for an amount of 5000/- rupees"
+
+
+# data1 = "Use OTP {} for your purchase at {} for an amount of {}/- rupees".format('728718','Amazon.com','8298')
+
+
+# print(data1)
+
+# data1 = "Use OTP {} for your purchase at {} for an amount of {}/- rupees".format('Amazon.com','8298','828919')
+
+# print(data1)
+
+
+# data1 = "Use OTP {otp} for your purchase at {website} for an amount of {amount}/- rupees".format(website = 'Amazon.com',amount = '8298',otp='828919')
+
+# print(data1)
+
+
+# f-fromatting
+
+otp = input("ENter your OTP:")
+website = input("Enter website:")
+amount = input("Enter Amount:")
+
+# data1 = "Use OTP {otp} for your purchase at {website} for an amount of {amount}/- rupees".format(website = 'Amazon.com',amount = '8298',otp='828919')
+
+
+data1 = f"Use OTP {otp} for your purchase at {website} for an amount of {amount}/- rupees"
+
+print(data1)
